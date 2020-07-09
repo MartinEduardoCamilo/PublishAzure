e9e5896ebdb4c1701d37f6479ffbd83997f73aae
Martin Eduardo Camilo De La Cruz <martin_camilo@ucne.edu.do>
2020-07-09 08:46:45 -0400

    Add project files.

---
+++ FunctionApp/CovidFAQ.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using System.Threading.Tasks;
+using FunctionApp.Models;
+
+namespace FunctionApp
+{
+    class CovidFAQ
+    {
+        [FunctionName("covidFAQ")]
+        public static async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+        [CosmosDB(
+            databaseName:"MDB",
+            collectionName:"FAQContainer",
+            ConnectionStringSetting = "DBConnectionString"
+            )] IEnumerable<FAQ> questionSet,
+        ILogger log)
+        {
+            log.LogInformation("Data fetched from FAQContainer");
+            return new OkObjectResult(questionSet);
+        }
+    }
+}
